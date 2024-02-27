c4d8499de6bca5a15475e60b5a411476917aa0b6
Siyuan Li <78656349+ApolloLSY@users.noreply.github.com>
2024-02-28 00:43:05 +0800

    Add files via upload

---
+++ commontraits/commontrait_ASD/model_test.py
@@ -0,0 +1,121 @@
+# %%
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from joblib import load
+from sklearn.linear_model import LassoCV
+from sklearn.metrics import auc, roc_auc_score, roc_curve
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+
+# %%
+def check_and_fill_cpg(data, selected_cpg):
+    for col in selected_cpg:
+        if col not in data.columns:
+            print(
+                f"Column {col} does not exist in the DataFrame. Adding and fill it now (FYI: fill with 0.5)."
+            )
+            data[col] = 0.5
+        else:
+            print(f"Column {col} exists in the DataFrame.")
+
+    return data
+
+
+# %%
+import warnings
+
+import pandas as pd
+from joblib import dump
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.metrics import accuracy_score, roc_auc_score
+from sklearn.model_selection import train_test_split
+
+warnings.filterwarnings("ignore")
+
+
+# load data
+datafile = pd.read_table(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/data_test.txt",
+    index_col=0,
+    header=0,
+    delimiter="\t",
+)
+
+
+# isna
+has_nan = datafile.isna().any().any()
+print("Any NaN values present:", has_nan)
+
+
+if has_nan:
+    # NaN20
+    datafile.fillna(0, inplace=True)
+
+    # check again
+    has_nan_after_fillna = datafile.isna().any().any()
+    if not has_nan_after_fillna:
+        print("All NaN values replaced with 0.")
+    else:
+        print("Some NaN values still present failing to replace with 0.")
+else:
+    print("No NaN values found.")
+
+
+# %%
+# Sample Exclusions: Samples excluded if ≥1% of CpGs had a detection p-value exceeding 0.05
+outlier_threshold2 = datafile.drop("status", axis=1).mean() + 1.96 * datafile.drop(
+    "status", axis=1
+).std() / np.sqrt(len(datafile))
+columns_to_drop = []
+for cpg_column in datafile.columns.drop("status"):
+    num_outliers = (datafile[cpg_column] > outlier_threshold2[cpg_column]).sum()
+    if num_outliers > 0.01 * datafile.shape[1]:
+        columns_to_drop.append(cpg_column)
+data = datafile.drop(columns=columns_to_drop)
+
+X_test = data.drop("status", axis=1)
+y_test = data["status"]
+
+
+# %%
+loaded_model = load("/data/LSY/z_preparing_and_parts/commontrait_ASD/model.joblib")
+selected_features = load(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/model_selected_features.joblib"
+)
+
+X_test = check_and_fill_cpg(X_test, selected_features)
+X_test_selected = X_test[selected_features]
+
+predictions = loaded_model.predict(X_test_selected)
+
+
+# %%
+
+auc = roc_auc_score(y_test, predictions)
+accuracy = accuracy_score(y_test, predictions)
+
+
+# %%
+
+test_results = pd.DataFrame(
+    {"Index": X_test.index, "Predicted": predictions, "Actual": y_test}
+)
+
+test_results.to_csv(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/prediction_test.txt",
+    sep="\t",
+    index=False,
+    mode="a",
+)
+
+test_scores = pd.DataFrame({"auc": [auc], "accuracy": [accuracy]}, index=["test"])
+
+test_scores.to_csv(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/performance.txt",
+    mode="a",
+    index=True,
+)
---
+++ commontraits/commontrait_ASD/prepare_data_valid.py
@@ -0,0 +1,148 @@
+import pandas as pd
+
+
+def m_to_beta(M):
+    return 1 / (1 + 2 ** (-M))
+
+
+# read matrix
+with open(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/GSE108785_series_matrix.txt", "r"
+) as file:
+    lines = file.readlines()
+
+# find metadata
+first_empty_line_index = lines.index("\n")
+table_begin_index = next(
+    (
+        i
+        for i, line in enumerate(lines)
+        if line.startswith("!series_matrix_table_begin")
+    ),
+    None,
+)
+end_index = table_begin_index if table_begin_index is not None else len(lines)
+content_before_table = lines[first_empty_line_index:end_index]
+
+
+# read metadata
+metadata_lines = [
+    line.strip()[1:].split("\t")
+    for line in content_before_table
+    if line.startswith("!")
+]
+metadata_df = pd.DataFrame(metadata_lines).T
+
+
+metadata_df.columns = metadata_df.iloc[0]
+metadata_df = metadata_df.drop(0, axis=0)
+
+metadata_df.dropna(inplace=True)
+
+
+# find status
+metadata_df["status"] = metadata_df.iloc[:, 10].apply(
+    lambda x: 1 if "autism" in x else 0 if "control" in x else None
+)
+metadata_df = metadata_df.set_index("Sample_geo_accession")
+
+metadata_df = metadata_df["status"].apply(pd.to_numeric, errors="coerce")
+
+
+# read data
+data_lines = [line.strip().split() for line in lines if not line.startswith("!")]
+data_df = pd.DataFrame(data_lines).T
+
+data_df.columns = data_df.iloc[0]
+data_df = data_df.drop(0, axis=0)
+
+data_df = data_df.set_index('"ID_REF"')
+data_df = data_df.iloc[:, 1:]
+data_df.columns = [col.strip('"') for col in data_df.columns]
+
+data_df = data_df.apply(pd.to_numeric, errors="coerce")
+
+
+# merge metadata and data
+data_df["status"] = metadata_df
+# drop other disease sample
+data_df.dropna(subset=["status"], inplace=True)
+
+data1 = data_df
+
+
+# read matrix
+with open(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/GSE109042_series_matrix.txt", "r"
+) as file:
+    lines = file.readlines()
+
+# find metadata
+first_empty_line_index = lines.index("\n")
+table_begin_index = next(
+    (
+        i
+        for i, line in enumerate(lines)
+        if line.startswith("!series_matrix_table_begin")
+    ),
+    None,
+)
+end_index = table_begin_index if table_begin_index is not None else len(lines)
+content_before_table = lines[first_empty_line_index:end_index]
+
+
+# read metadata
+metadata_lines = [
+    line.strip()[1:].split("\t")
+    for line in content_before_table
+    if line.startswith("!")
+]
+metadata_df = pd.DataFrame(metadata_lines).T
+
+
+metadata_df.columns = metadata_df.iloc[0]
+metadata_df = metadata_df.drop(0, axis=0)
+
+metadata_df.dropna(inplace=True)
+
+
+# find status
+metadata_df["status"] = metadata_df.iloc[:, 12].apply(
+    lambda x: 1 if "FASD" in x else 0 if "Control" in x else None
+)
+metadata_df = metadata_df.set_index("Sample_geo_accession")
+
+metadata_df = metadata_df["status"].apply(pd.to_numeric, errors="coerce")
+
+
+# read data
+data_lines = [line.strip().split() for line in lines if not line.startswith("!")]
+data_df = pd.DataFrame(data_lines).T
+
+data_df.columns = data_df.iloc[0]
+data_df = data_df.drop(0, axis=0)
+
+data_df = data_df.set_index('"ID_REF"')
+data_df = data_df.iloc[:, 1:]
+data_df.columns = [col.strip('"') for col in data_df.columns]
+
+data_df = data_df.apply(pd.to_numeric, errors="coerce")
+
+# data_df = data_df.apply(m_to_beta)
+
+
+# merge metadata and data
+data_df["status"] = metadata_df
+# drop other disease sample
+data_df.dropna(subset=["status"], inplace=True)
+
+concatenated_data = pd.concat([data1, data_df], ignore_index=True)
+
+
+# save data
+concatenated_data.to_csv(
+    "/data/LSY/z_preparing_and_parts/commontrait_ASD/data_valid.txt",
+    index=True,
+    header=True,
+    sep="\t",
+)
